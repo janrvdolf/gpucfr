22e33cfe0877a4f3952da4a8794d7df9f4a91e44
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2021-01-13 15:08:49 +0100

    Refactors the main file.

--- main3.cu
+++ main3.cu
@@ -8,74 +8,58 @@ int main () {
     unsigned int iterations = 1000;
     GPUCFR instance1 = GPUCFR("../gs2.game");
     instance1.run_iterations(iterations);
-    instance1.export_strategy_for_gtlib("gs2.strategy");
+    instance1.export_strategy_for_gtlib("gs2.strategy"); // TODO
     std::cout << "1000 iterations takes " << instance1.elapsed_time() << "ms on Goospiel 2" << std::endl;
 
-//    iterations = 5000;
-//    GPUCFR game_loader2 = GPUCFR("../gs2.game");
-//    game_loader2.load();
-//    game_loader2.memcpy_host_to_gpu();
-//    game_loader2.run_iterations(iterations);
-//    game_loader2.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader2.elapsed_time() << "ms on Goospiel 2" << std::endl;
-//
-//    iterations = 10000;
-//    GPUCFR game_loader3 = GPUCFR("../gs2.game");
-//    game_loader3.load();
-//    game_loader3.memcpy_host_to_gpu();
-//    game_loader3.run_iterations(iterations);
-//    game_loader3.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader3.elapsed_time() << "ms on Goospiel 2" << std::endl;
-//
-//    /* Goofspiel 3 */
-//    iterations = 1000;
-//    GPUCFR game_loader4 = GPUCFR("../gs3.game");
-//    game_loader4.load();
-//    game_loader4.memcpy_host_to_gpu();
-//    game_loader4.run_iterations(iterations);
-//    game_loader4.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader4.elapsed_time() << "ms on Goospiel 3" << std::endl;
-//
-//    iterations = 5000;
-//    GPUCFR game_loader5 = GPUCFR("../gs3.game");
-//    game_loader5.load();
-//    game_loader5.memcpy_host_to_gpu();
-//    game_loader5.run_iterations(iterations);
-//    game_loader5.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader5.elapsed_time() << "ms on Goospiel 3" << std::endl;
-//
-//    iterations = 10000;
-//    GPUCFR game_loader6 = GPUCFR("../gs3.game");
-//    game_loader6.load();
-//    game_loader6.memcpy_host_to_gpu();
-//    game_loader6.run_iterations(iterations);
-//    game_loader6.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader6.elapsed_time() << "ms on Goospiel 3" << std::endl;
-//
-//    /* Goofspiel 3 */
-//    iterations = 1000;
-//    GPUCFR game_loader7 = GPUCFR("../gs4.game");
-//    game_loader7.load();
-//    game_loader7.memcpy_host_to_gpu();
-//    game_loader7.run_iterations(iterations);
-//    game_loader7.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader7.elapsed_time() << "ms on Goospiel 4" << std::endl;
-//
-//    iterations = 5000;
-//    GPUCFR game_loader8 = GPUCFR("../gs4.game");
-//    game_loader8.load();
-//    game_loader8.memcpy_host_to_gpu();
-//    game_loader8.run_iterations(iterations);
-//    game_loader8.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader8.elapsed_time() << "ms on Goospiel 4" << std::endl;
-//
-//    iterations = 10000;
-//    GPUCFR game_loader9 = GPUCFR("../gs4.game");
-//    game_loader9.load();
-//    game_loader9.memcpy_host_to_gpu();
-//    game_loader9.run_iterations(iterations);
-//    game_loader9.memcpy_gpu_to_host();
-//    std::cout << "1000 iterations takes " << game_loader9.elapsed_time() << "ms on Goospiel 4" << std::endl;
-//
+    iterations = 5000;
+    GPUCFR instance2 = GPUCFR("../gs2.game");
+    instance2.load();
+    instance2.run_iterations(iterations);
+    std::cout << "5000 iterations takes " << instance2.elapsed_time() << "ms on Goospiel 2" << std::endl;
+
+    iterations = 10000;
+    GPUCFR instance3 = GPUCFR("../gs2.game");
+    instance3.load();
+    instance3.run_iterations(iterations);
+    std::cout << "10000 iterations takes " << instance3.elapsed_time() << "ms on Goospiel 2" << std::endl;
+
+    /* Goofspiel 3 */
+    iterations = 1000;
+    GPUCFR instance4 = GPUCFR("../gs3.game");
+    instance4.load();
+    instance4.run_iterations(iterations);
+    std::cout << "1000 iterations takes " << instance4.elapsed_time() << "ms on Goospiel 3" << std::endl;
+
+    iterations = 5000;
+    GPUCFR instance5 = GPUCFR("../gs3.game");
+    instance5.load();
+    instance5.run_iterations(iterations);
+    std::cout << "5000 iterations takes " << instance5.elapsed_time() << "ms on Goospiel 3" << std::endl;
+
+    iterations = 10000;
+    GPUCFR instance6 = GPUCFR("../gs3.game");
+    instance6.load();
+    instance6.run_iterations(iterations);
+    std::cout << "10000 iterations takes " << instance6.elapsed_time() << "ms on Goospiel 3" << std::endl;
+
+    /* Goofspiel 3 */
+    iterations = 1000;
+    GPUCFR instance7 = GPUCFR("../gs4.game");
+    instance7.load();
+    instance7.run_iterations(iterations);
+    std::cout << "1000 iterations takes " << instance7.elapsed_time() << "ms on Goospiel 4" << std::endl;
+
+    iterations = 5000;
+    GPUCFR instance8 = GPUCFR("../gs4.game");
+    instance8.load();
+    instance8.run_iterations(iterations);
+    std::cout << "1000 iterations takes " << instance8.elapsed_time() << "ms on Goospiel 4" << std::endl;
+
+    iterations = 10000;
+    GPUCFR instance9 = GPUCFR("../gs4.game");
+    instance9.load();
+    instance9.run_iterations(iterations);
+    std::cout << "10000 iterations takes " << instance9.elapsed_time() << "ms on Goospiel 4" << std::endl;
+
     return 0;
 }
