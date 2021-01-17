16ea96822e296491aa95353f627b6a0282b983b2
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2021-01-17 16:18:22 +0100

    Refactor main.cu.

--- main.cu
+++ main.cu
@@ -4,9 +4,8 @@
 
 
 int main (int argc, char ** argv) {
-
     if (argc != 3) {
-        std::cerr << "Run the program with two parameters - <int: number of iterations> <string: efg game file>" << std::endl;
+        std::cerr << "Run the program with two parameters - <int: number of iterations> <string: efg game file path>" << std::endl;
         return 1;
     }
     unsigned int iterations = strtol(argv[1], nullptr, 0);
